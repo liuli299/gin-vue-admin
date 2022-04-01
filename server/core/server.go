08dc24467ad9b2b9b2f6694a47a50b99293cbeab
liuli299 <liuli299@163.com>
2022-04-01 10:18:27 +0800

    修改输出文案

--- server/core/server.go
+++ server/core/server.go
@@ -35,14 +35,6 @@ func RunWindowsServer() {
 	time.Sleep(10 * time.Microsecond)
 	global.LOG.Info("server run success on ", zap.String("address", address))
 
-	fmt.Printf(`
-	欢迎使用 admin/server
-	当前版本:V2.5.1 beta
-    加群方式:微信号：shouzi_1994 QQ群：622360840
-	GVA讨论社区:https://support.qq.com/products/371961
-	默认自动化文档地址:http://127.0.0.1%s/swagger/index.html
-	默认前端文件运行地址:http://127.0.0.1:8080
-	如果项目让您获得了收益，希望您能请团队喝杯可乐:https://www.admin/server.com/docs/coffee
-`, address)
+	fmt.Printf("系统启动成功，端口为：%s\n", address)
 	global.LOG.Error(s.ListenAndServe().Error())
 }
